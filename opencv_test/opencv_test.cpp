97cc8232311a79266bdf1e4ec3f05b6b30c840b5
Joossss <jospa44@gmail.com>
2017-12-22 02:25:29 +0200

    Refactoring

--- opencv_test/opencv_test.cpp
+++ opencv_test/opencv_test.cpp
@@ -22,6 +22,13 @@ Vec3b NewThresholdsLow(Vec3b old_val, Vec3b new_val);
 Vec3b NewThresholdsUp(Vec3b old_val, Vec3b new_val);
 void FingerMovement(vector<FINGER> &old_fingers, vector<FINGER> new_fingers);
 void draw_numbers(vector<FINGER> old_fingers, Mat &frame);
+void draw_biggest(vector<vector<Point>> &contours, int &num_fingers, Mat &out);
+void find_sort_contours(Mat &out, vector<vector<Point>> &contours);
+void first_lclick(Vec3b &lower_bound, Vec3b &upper_bound, Vec3b new_color);
+void left_mouse(Vec3b &lower_bound, Vec3b &upper_bound);
+void first_rclick(int num_fingers, vector<vector<Point>> contours, vector<FINGER> &old_fingers);
+void right_mouse(int num_fingers, vector<vector<Point>> contours, vector<FINGER> &old_fingers, vector<FINGER> &new_fingers, Mat &out);
+
 
 Mat frame;
 Point pt;
@@ -52,29 +59,10 @@ int main()
 	setMouseCallback("Stuff and stuff", CallBackFunc, NULL);
 
 	cap >> frame;
-	cout << frame.size << endl;
 
 	//Thresholding color for first run
 	Vec3b lower_bound = (0, 0, 0);
 	Vec3b upper_bound = (179, 255, 255);
-
-	//Blob detector parameters
-	SimpleBlobDetector::Params params;
-
-	//Filter by area
-	params.filterByArea = true;
-	params.minArea = 1000;
-	params.maxArea = 6000;
-
-	//Filter by color
-	params.filterByColor = true;
-	params.blobColor = 255;
-
-	//Not filtering by
-
-	params.filterByCircularity = false;
-	params.filterByConvexity = false;
-	params.filterByInertia = false;
 
 	//Fingers
 	int num_fingers = 4;
@@ -84,117 +72,42 @@ int main()
 	//For blurring
 	int blurSize = 5;
 
-	RNG rng(12345);
 
 	while (1)
 	{
+
+		//Variables
+		Mat out;
+		vector<vector<Point> > contours;
+
+		//Frame conversions
 		cap >> frame;
 		cvtColor(frame, frame, CV_BGR2RGBA);
 		cvtColor(frame, frame, CV_RGBA2RGB);
 		cvtColor(frame, frame, CV_RGB2HSV);
 
-		Mat out;
-
+		
+		//Thresholding and blurring
 		inRange(frame, lower_bound, upper_bound, out);
-
-
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-
 		medianBlur(out, out, blurSize);
 
-		/// Detect edges using canny
-		//Canny(out, out, 100, 200, 3);
-		/// Find contours
-		findContours(out, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+		 
+		/// Find contours and draw 
+		find_sort_contours(out, contours);
+		draw_biggest(contours, num_fingers, out);
 
-
-		sort(contours.begin(), contours.end(), [](const vector<Point>& c1, const vector<Point>& c2) {
-			return contourArea(c1, false) < contourArea(c2, false);
-		});
-		Mat drawing = Mat::zeros(out.size(), CV_8UC3);
-
-		for (int i = 0; i < contours.size(); i++) {
-			drawContours(drawing, contours, i, Scalar(255, 255, 255), 2, 8);
-		}
-		vector<vector<Point>> big_cont;
-		num_fingers = 4;
-		for (int i = 0; i < contours.size(); i++) {
-			if(contourArea(contours[contours.size()-i-1]) > 100)
-				big_cont.push_back(contours[contours.size()-i-1]);
-		}
-		contours = big_cont;
-		num_fingers = MIN(num_fingers, contours.size());
-		/// Draw contours
-		for (int i = 0; i < num_fingers; i++) {
-			drawContours(drawing, contours, i, Scalar(255,0, 0 ), 2, 8);
-		}
+		//Draw all contours for debugging
+		/*for (int i = 0; i < contours.size(); i++) {
+			drawContours(out, contours, i, Scalar(255, 255, 255), 2, 8);
+		}*/
 		
 		
-		//Mouseclick routine
-		if (lclicked) {
-			Vec3b new_color = frame.at<Vec3b>(pt.y, pt.x);
-			cout << new_color << endl;
-
-			//If it's the first mouseclick of the session
-			if (first_L) {
-				lower_bound[0] = uint8_t(abs(new_color[0] - THRESH_HUE));
-				lower_bound[1] = uint8_t(abs(new_color[1] - THRESH_SAT));
-				lower_bound[2] = uint8_t(abs(new_color[2] - THRESH_VAL));
-				if (new_color[0] + THRESH_HUE < 180) {
-					upper_bound[0] = uint8_t(new_color[0] + THRESH_HUE);
-				}
-				else {
-					upper_bound[0] = 179;
-				}
-				if (new_color[1] + THRESH_SAT < 256) {
-					upper_bound[1] = uint8_t(new_color[1] + THRESH_SAT);
-				}
-				else {
-					upper_bound[1] = 255;
-				}
-				if (new_color[2] + THRESH_VAL < 256) {
-					upper_bound[2] = uint8_t(new_color[2] + THRESH_VAL);
-				}
-				else {
-					upper_bound[2] = 255;
-				}
-				first_L = 0;
-			}
-			else {
-				lower_bound = NewThresholdsLow(lower_bound, new_color);
-				upper_bound = NewThresholdsUp(upper_bound, new_color);
-			}
-			cout << "Lower bound: " << lower_bound << ", upper bound: " << upper_bound << endl;
-			lclicked = 0;
-		}
-		if (rclicked) {
-			if (first_R) {
-				for (int i = 0; i < contours.size(); i++) {
-					Moments M = moments(contours[i], false);
-					Point P = Point(round(M.m10 / M.m00), round(M.m01 / M.m00));
-					old_fingers[i].x = P.x;
-					old_fingers[i].y = P.y;
-					old_fingers[i].key_pushed = false;
-					old_fingers[i].ID = i;
-				}
-				first_R = 0;
-			}
-			else {
-				for (int i = 0; i < contours.size(); i++) {
-					Moments M = moments(contours[i], false);
-					Point P = Point(round(M.m10 / M.m00), round(M.m01 / M.m00));
-					new_fingers[i].x = P.x;
-					new_fingers[i].y = P.y;
-					new_fingers[i].key_pushed = false;
-				}
-				FingerMovement(old_fingers, new_fingers);
-			}
-			draw_numbers(old_fingers, drawing);
-		}
+		//Mouseclick routines
+		left_mouse(lower_bound, upper_bound);
+		right_mouse(num_fingers, contours, old_fingers, new_fingers, out);
 
 		cv::imshow("Stuff and stuff", frame);
-		cv::imshow("threshed", drawing);
+		cv::imshow("threshed", out);
 
 		if (waitKey(10) >= 0)
 			break;
@@ -298,12 +211,12 @@ Vec3b NewThresholdsUp(Vec3b old_val, Vec3b new_val) {
 	return upper_bound;
 }
 
+//Track the contours
 void FingerMovement(vector<FINGER> &old_fingers, vector<FINGER>new_fingers) {
 	for (int i = 0; i < old_fingers.size(); i++) {
 		FINGER holder = new_fingers[0];
 		int chosen = 0;
 		for (int j = 0; j < new_fingers.size(); j++) {
-			cout << "i: " << i << endl;
 			if (abs(new_fingers[j].x) < 1280 && abs(new_fingers[j].y) < 720) {
 				float dxn = pow((new_fingers[j].x - old_fingers[j].x), 2);
 				float dyn = pow((new_fingers[j].y - old_fingers[j].y), 2);
@@ -321,14 +234,115 @@ void FingerMovement(vector<FINGER> &old_fingers, vector<FINGER>new_fingers) {
 	}
 }
 
+//Draw id numbers for each contour
 void draw_numbers(vector<FINGER> old_fingers, Mat &frame) {
 	for (int i = 0; i < old_fingers.size(); i++) {
 		putText(frame, to_string(old_fingers[i].ID), cvPoint(old_fingers[i].x, old_fingers[i].y),
 			FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(255, 0 ,0), 1, CV_AA);
-		cout << "ID: " << old_fingers[i].ID << ", x: " << old_fingers[i].x << ", y:" << old_fingers[i].y << endl;
 
 	}
 	//return frame;
+}
+
+//Draw the biggest contours
+void draw_biggest(vector<vector<Point>> &contours, int &num_fingers, Mat &out){
+	out = Mat::zeros(out.size(), CV_8UC3);
+	num_fingers = 4;
+	vector<vector<Point>> big_cont;
+	for (int i = 0; i < contours.size(); i++) {
+		if (contourArea(contours[contours.size() - i - 1]) > 100)
+			big_cont.push_back(contours[contours.size() - i - 1]);
+	}
+	contours = big_cont;
+	num_fingers = MIN(num_fingers, contours.size());
+	/// Draw contours
+	for (int i = 0; i < num_fingers; i++) {
+		drawContours(out, contours, i, Scalar(255, 0, 0), 2, 8);
+	}
+}
+
+//detect and sort the contours in respect to size in ascending order
+void find_sort_contours(Mat &out, vector<vector<Point>> &contours){
+	findContours(out, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	sort(contours.begin(), contours.end(), [](const vector<Point>& c1, const vector<Point>& c2) {
+		return contourArea(c1, false) < contourArea(c2, false);
+	});
+	}
+
+//Left mouse first click
+void first_lclick(Vec3b &lower_bound, Vec3b &upper_bound, Vec3b new_color) {
+	lower_bound[0] = uint8_t(abs(new_color[0] - THRESH_HUE));
+	lower_bound[1] = uint8_t(abs(new_color[1] - THRESH_SAT));
+	lower_bound[2] = uint8_t(abs(new_color[2] - THRESH_VAL));
+	if (new_color[0] + THRESH_HUE < 180) {
+		upper_bound[0] = uint8_t(new_color[0] + THRESH_HUE);
+	}
+	else {
+		upper_bound[0] = 179;
+	}
+	if (new_color[1] + THRESH_SAT < 256) {
+		upper_bound[1] = uint8_t(new_color[1] + THRESH_SAT);
+	}
+	else {
+		upper_bound[1] = 255;
+	}
+	if (new_color[2] + THRESH_VAL < 256) {
+		upper_bound[2] = uint8_t(new_color[2] + THRESH_VAL);
+	}
+	else {
+		upper_bound[2] = 255;
+	}
+	first_L = 0;
+}
+
+//Left mouse
+void left_mouse(Vec3b &lower_bound, Vec3b &upper_bound){
+	if (lclicked) {
+		Vec3b new_color = frame.at<Vec3b>(pt.y, pt.x);
+
+		//If it's the first mouseclick of the session
+		if (first_L)
+			first_lclick(lower_bound, upper_bound, new_color);
+		else {
+			lower_bound = NewThresholdsLow(lower_bound, new_color);
+			upper_bound = NewThresholdsUp(upper_bound, new_color);
+		}
+		lclicked = 0;
+	}
+}
+
+//Right mouse first click
+void first_rclick(int num_fingers, vector<vector<Point>> contours, vector<FINGER> &old_fingers) {
+	for (int i = 0; i < num_fingers; i++) {
+		Moments M = moments(contours[i], false);
+		Point P = Point(round(M.m10 / M.m00), round(M.m01 / M.m00));
+		old_fingers[i].x = P.x;
+		old_fingers[i].y = P.y;
+		old_fingers[i].key_pushed = false;
+		old_fingers[i].ID = i;
+	}
+	first_R = 0;
+}
+
+//Right mouse - this will run everytime after first click
+void right_mouse(int num_fingers, vector<vector<Point>> contours, vector<FINGER> &old_fingers, vector<FINGER> &new_fingers, Mat &out) {
+	if (rclicked) {
+		if (first_R) {
+			first_rclick(num_fingers, contours, old_fingers);
+		}
+		else {
+			for (int i = 0; i < num_fingers; i++) {
+				Moments M = moments(contours[i], false);
+				Point P = Point(round(M.m10 / M.m00), round(M.m01 / M.m00));
+				new_fingers[i].x = P.x;
+				new_fingers[i].y = P.y;
+				new_fingers[i].key_pushed = false;
+			}
+			FingerMovement(old_fingers, new_fingers);
+		}
+		draw_numbers(old_fingers, out);
+	}
 }
 
 
